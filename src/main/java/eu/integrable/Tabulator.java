5ee2142a905a14c4d21b0e91facc0c39695510cf
Kamil Szewc <kamil.szewc@gmail.com>
2022-03-02 17:04:35 +0100

    Testing deployment

--- src/main/java/eu/integrable/Tabulator.java
+++ src/main/java/eu/integrable/Tabulator.java
@@ -1,12 +1,62 @@
 package eu.integrable;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Tabulator {
 
+    private Class aClass;
+    private Object object;
+
     public Tabulator(Object object) {
-        System.out.println(object.getClass());
+
+        this.aClass = object.getClass();
+        this.object = object;
+    }
+
+    private static List<Method> getAllGetMethods(Class aClass) {
+        return Arrays.stream(aClass.getDeclaredMethods())
+                .filter(c -> c.getName().startsWith("get"))
+                .sorted((a, b) -> a.getName().compareTo(b.getName()) )
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private static Map<String, String> getMapOfMethodNameAndValue(Object object) {
+        Map<String, String> map = new LinkedHashMap<>();
+
+        List<Method> methods = getAllGetMethods(object.getClass());
+        methods.stream().forEach(method -> {
+            try {
+                String name = method.getName().replaceFirst("get", "");
+                StringBuilder sb = new StringBuilder(name);
+                sb.setCharAt(0, Character.toLowerCase(sb.charAt(0)));
+                name = sb.toString();
+                map.put(name, method.invoke(object).toString());
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        });
+
+        return map;
+    }
+
+    public String getJson() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        return objectMapper.writeValueAsString(object);
     }
 
     public String getTable() {
-        return "table";
+        return "will be developed soon";
     }
+
+
+
 }
--- src/test/java/eu/integrable/TabulatorTest.java
+++ src/test/java/eu/integrable/TabulatorTest.java
@@ -1,8 +1,12 @@
 package eu.integrable;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
 
 public class TabulatorTest {
 
@@ -15,10 +19,12 @@ public class TabulatorTest {
     public static class Basic {
         private Long first;
         private String second;
+        @Builder.Default
+        private LocalDateTime time = LocalDateTime.now();
     }
 
     @Test
-    public void convertingBasicClassToTable() {
+    public void convertingBasicClassToTable() throws JsonProcessingException {
 
         Basic basic = Basic.builder()
                 .first(1L)
@@ -27,6 +33,7 @@ public class TabulatorTest {
 
         Tabulator tabulator = new Tabulator(basic);
         System.out.println(tabulator.getTable());
+        System.out.println(tabulator.getJson());
 
         Assertions.assertEquals(1, 1);
     }
